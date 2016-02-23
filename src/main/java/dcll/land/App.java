2b7f018f53d8abc1c70775820e03b7d5dd55472d
lovajenny <lovajenny.andriana@gmail.com>
2016-02-23 21:07:02 +0100

    Modif groupId

--- src/main/java/dcll/land/App.java
+++ src/main/java/dcll/land/App.java
@@ -1,4 +1,4 @@
-package dcll.mcie;
+package dcll.land;
 
 /**
  * Hello world!
--- src/test/java/dcll/land/AppTest.java
+++ src/test/java/dcll/land/AppTest.java
@@ -1,4 +1,4 @@
-package dcll.mcie;
+package dcll.land;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
